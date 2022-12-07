f49db7251451d4ac92d9540ed38b184c72f87f49
sachikavitake <74591026+sachikavitake@users.noreply.github.com>
2022-12-07 22:38:54 +0530

    remove resolution to have rectangle images

--- lucidsonicdreams/main.py
+++ lucidsonicdreams/main.py
@@ -630,11 +630,11 @@ class LucidSonicDream:
             # final_image = Image.fromarray(array, 'RGB')
 
 #             # If resolution is provided, resize
-            if resolution:
-              final_image = cv2.resize(array,resolution,cv2.INTER_CUBIC)
-            else:
-              final_image = array[:,:,::-1]
-
+#             if resolution:
+#               final_image = cv2.resize(array,resolution,cv2.INTER_CUBIC)
+#             else:
+            final_image = array[:,:,::-1]
+            
             video_out.write(final_image)
     video_out.release()
                 # final_image = final_image.resize((resolution, resolution))
